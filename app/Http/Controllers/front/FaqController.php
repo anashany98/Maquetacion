94e327f318af44aae1abb3c420e97fc45292ca5f
anashany98 <anashanyah@gmail.com>
2021-05-11 15:13:43 +0200

    Front language

--- app/Http/Controllers/front/FaqController.php
+++ app/Http/Controllers/front/FaqController.php
@@ -6,35 +6,45 @@ use Illuminate\Support\Facades\View;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\DB\Faq;
+use Jenssegers\Agent\Agent;
+use App\Vendor\Locale\Locale;
+use App;
+use Debugbar;
 
 class FaqController extends Controller
 {
     protected $faq;
+    protected $agent;
+    protected $locale;
 
-    function __construct(Faq $faq)
+    function __construct(Faq $faq, Locale $locale, Agent $agent)
     {
         $this->faq = $faq;
+        $this->agent = $agent;
+        $this->locale = $locale;
+
+        $this->locale->setParent('faqs');
+        $this->locale->setLanguage(App::getLocale());
+
     }
 
     public function index()
     {
+        $faqs = $this->faq->where('active', 1)->get();
+
+        $faqs = $faqs->each(function($faq){
+
+            $faq['locale'] = $faq->locale->pluck('value', 'tag');
+
+            return $faq;
+            
+        });
 
         $view = View::make('front.faqs.index')
-                ->with('faqs', $this->faq->where('active', 1)->get());
+                ->with('faqs', $faqs );
 
-        if(request()->ajax()) {
-
-            // $faqs = $this->faq->where('active', 1)->get();
-            // return response()->json($faqs);
-            
-            // $sections = $view->renderSections(); 
-    
-            // return response()->json([
-            //     'table' => $sections['table'],
-            //     'form' => $sections['form'],
-            // ]); 
-        }
 
         return $view;
+
     }
 }
